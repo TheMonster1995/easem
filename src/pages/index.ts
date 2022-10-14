7fded4dd30cc3af9f46bc419e328815bffe290e9
Fo'ad (Mnstr) Salmanian <salmanian.foad2@gmail.com>
2022-10-14 07:15:01 -0700

    Add files via upload

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Login, ResetPassword } from './Login';
+export { default as OrderManager } from './Dashboard/OrderManager';
+export { default as ItemManager } from './Dashboard/ItemManager';
+export { default as Report } from './Dashboard/Report';
+export { default as Customers } from './Dashboard/Customers';
